ccd469a93e89eab54f19e7c9fcc22d986180c9fc
rcidral <ricidralmfo@gmail.com>
2023-02-13 20:38:21 -0300

    adding the final code, all done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace NamespaceProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recebendo arquivo...");
+            string notOrdened = System.IO.File.ReadAllText("./assets/notOrdened.txt");
+            Console.WriteLine("Arquivo recebido com sucesso!");
+            
+            Console.WriteLine("Definindo tamanho do array em relação a quantidade de linhas do arquivo...");
+            string [] array = notOrdened.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            Console.WriteLine("Tamanho do array definido com sucesso!");
+            Console.WriteLine("Inserindo valores do arquivo no array...");
+            int [] arrayInt = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                arrayInt[i] = int.Parse(array[i]);
+            }
+            Console.WriteLine("Valores inseridos com sucesso!");
+            Console.WriteLine("Ordenando array...");
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                for (int j = 0; j < arrayInt.Length; j++)
+                {
+                    if (arrayInt[i] < arrayInt[j])
+                    {
+                        int aux = arrayInt[i];
+                        arrayInt[i] = arrayInt[j];
+                        arrayInt[j] = aux;
+                    }
+                }
+            }
+            Console.WriteLine("Array ordenado com sucesso!");
+            Console.WriteLine("Mostando array ordenado...");
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                Console.WriteLine(arrayInt[i]);
+            }
+
+            Console.WriteLine("Salvando valores ordenados em uma Collection...");
+            List<int> list = new List<int>();
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                list.Add(arrayInt[i]);
+            }
+            Console.WriteLine("Valores salvos com sucesso na Colletion!");
+            
+            Console.WriteLine("Array ordenado: ");
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Collection ordenada: ");
+            Console.WriteLine(list);
+
+            Console.WriteLine("Salvando valores ordenados em um arquivo...");
+            System.IO.File.WriteAllLines("./assets/Ordened.txt", list.Select(x => x.ToString()));
+            Console.WriteLine("Valores salvos com sucesso em um arquivo!");
+
+
+
+           
+            
+            
+            
+
+
+
+
+
+
+            
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+No indicador 1. Analisa os elementos para programação desktop e cliente/servidor, que tem como critério a Análise, o objetivo é desenvolver a prática em Programação Orientada a Objetos em especial para sistemas Desktop, permitindo ao estudante aplicar uma linguagem de desenvolvimento orientado a objetos, de maneira a apresentar soluções de problemas no cotidiano, compreendendo a correlação com o sistema.
+
+Ainda, de maneira a correlacionar a atividade aplicada entre JAVA e C#, o objetivo da atividade proposta é se utilização da Relação como plataforma de conhecimento.
+
+Para analisar o aprendizado, desenvolva o modelo de ordenação proposto, utilizando as estruturas de Array propostas em sala de aula. O desenvolvimento deve conter:
+
+Comentários de Código demonstrando a estrutura
+Indentação de Código
+Utilização de Estruturas de Repetição para solução da ordenação
+Aplicação de camelCase e PascalCase para o código fonte
+Nomenclatura de variáveis que determinem sua utilização
+
+O programa deverá receber uma lista de valores de um arquivo de texto, onde esta lista não terá uma determinação de quantidade fixa, podendo variar de arquivo para arquivo. Após, deverá ser alocado os valores dentro da estrutura de Array (não poderão ser aplicadas Collections para solução), e realizada a ordenação dos valores, imprimindo em tela os passos ocorridos desta ordenação. Após, deverá ser utilizado uma Collection para o recebimento dos mesmos valores, de maneira que estes valores já estejam ordenados. Ao final, deverá ser impresso em tela o valor ordenado do Array e da Collection, garantindo que estejam iguais, e deverá ser exportado em um arquivo de texto esses valores de uma das duas estruturas.
